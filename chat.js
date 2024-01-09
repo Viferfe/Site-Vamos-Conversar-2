63aa3b036fe2b064d3a2347d4b4c95d8ebb121ad
Viferfe <142558392+Viferfe@users.noreply.github.com>
2024-01-09 20:15:40 -0300

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,28 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCEjFIqwI5xdXzKqikeKfQjdMMRX4cp7OA",
+  authDomain: "kwitter-parte-1.firebaseapp.com",
+  databaseURL: "https://kwitter-parte-1-default-rtdb.firebaseio.com",
+  projectId: "kwitter-parte-1",
+  storageBucket: "kwitter-parte-1.appspot.com",
+  messagingSenderId: "1016752520188",
+  appId: "1:1016752520188:web:2df5ce9dd948d995ff1079"
+};
+
+const app = initializeApp(firebaseConfig);
+
+
+function addUser()
+{
+    user_name = document.getElementById("user_name").value;
+    firebase.database().ref("/").child(user_name).update({
+        purpose : "adding user"
+    });
+    
+    localStorage.setItem("user_name", user_name);
+
+    window.location = "chat_room.html";
+}
+
+
+
